ee3dbe9a0376828d20bcc04d9a211324c5efafc7
jongwook <project5587@google.com>
2024-07-20 20:06:43 +0900

    Layout Update #22

--- components/Projects.js
+++ components/Projects.js
@@ -2,8 +2,27 @@ import styles from '../styles/Projects.module.css'
 
 function Projects() {
   return (
-    <div>
-      Projects
+    <div className={styles.projects} id="projects">
+      <div className={styles.contentBox}>
+        <div className={styles.titleBox}>
+          <div className={styles.title}>
+            PROJECTS
+          </div>
+        </div>
+        <div className={styles.projectsContainer}>
+          <div className={styles.project}>
+            <div className={styles.projectTitle}>
+              Project Title
+            </div>
+            <div className={styles.projectSubTitle}>
+              Project Subtitle
+            </div>
+            <div className={styles.projectDescription}>
+              Description
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
