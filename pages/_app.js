382bf370cdb9b727735cd2a92b9b4368f7e1d873
jongwook <project5587@google.com>
2024-08-07 01:37:16 +0900

    Update app.js

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,7 @@ export default function App({ Component, pageProps }) {
           <meta property="og:description" content="Jongwook, LEE Portfolio Web Site (feat. AI Chatbot)" />
           <meta property="og:image" content="https://raw.githubusercontent.com/Korea-Maker/My-Resume/main/public/favicon.ico" />
           <meta property="og:type" content="website" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
       </Head>
       <Layout>
         <Component {...pageProps} />
