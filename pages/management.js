2cbb02e304f4b5805a0343cd80cdc603df449568
jongwook <project5587@google.com>
2024-08-29 14:57:45 +0900

    Update Management Page

--- pages/management.js
+++ pages/management.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import axios from 'axios';
+import Styles from '../styles/Management.module.css';
 
 function Management() {
   const [authorized, setAuthorized] = useState(false);
